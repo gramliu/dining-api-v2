d32f82f375ad1504f38b8f195c0bd0678be150cb
David Ethan Hwang <hdavidethan@gmail.com>
2020-03-17 16:37:51 -0400

    Fixed express

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const cheerio = require('cheerio');
 const request = require('request');
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './src/routes/routes';
 
 const app = express();
 
@@ -22,3 +23,13 @@ function parseBody(body) {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+routes(app);
+
+app.get("/", function(req, res) {
+    res.send("ScottyLabs CourseAPI Homepage");
+});
+
+app.listen(port, () =>
+    console.log(`App listening on port ${port}.`)
+);
